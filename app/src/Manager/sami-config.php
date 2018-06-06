88a7e748a1e99350c68e8add6a867601022cb5b2
LouisCuny <louis-cuny@orange.fr>
2018-06-06 09:51:12 +0200

    Found how to pass vars into sami

--- app/src/Manager/sami-config.php
+++ app/src/Manager/sami-config.php
@@ -1,5 +1,4 @@
 <?php
-
 use Sami\Sami;
 use Symfony\Component\Finder\Finder;
 
@@ -12,12 +11,22 @@ $iterator = Finder::create()
     ->name('*.php')
     ->in($dir = $tmpDir . $infos->repoPath . '/src');
 
-return new Sami($iterator, [
+$sami = new Sami($iterator, [
     'theme'                => 'objectivephp-sami',
     'versions'             => $infos->version,
     'title'                => ucfirst($infos->compoName),
-    'build_dir'            =>  __DIR__ . '/../../../public/doc/' .$infos->compoName. '/%version%/api',
+    'build_dir'            => __DIR__ . '/../../../public/doc/' . $infos->compoName . '/%version%/api',
     'cache_dir'            => $tmpDir . '/cache/' . $infos->compoName . '/%version%',
     'template_dirs'        => [__DIR__ . '/../../../sami/' . 'objectivephp-sami'],
     'default_opened_level' => 1
 ]);
+
+$sami->extend('twig', function ($twig) {
+    $twig->addGlobal('UZ', 'TOTA');
+    return $twig;
+});
+
+//$sami->offsetGet('project')->update();
+
+return $sami;
+
